44c46022c40cda8df1839289e0b417c54acf9be0
ChrisScott9456 <christopherscott9456@gmail.com>
2022-02-17 17:02:55 -0500

    Set up basic css and routing with the icon banner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent }, // TODO: Remove this later - just for testing rerouting via banner
+  { path: '', component: SearchComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
